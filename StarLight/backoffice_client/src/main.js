b5b7667a2d37248e65ad40ca56a661699f6e6759
miawang96 <Minya_Wang@163.com>
2020-09-08 21:28:36 +0800

    Add UI framework.

--- StarLight/backoffice_client/src/main.js
+++ StarLight/backoffice_client/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import ViewUI from 'view-design';
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(ViewUI)
   .mount('#app');
